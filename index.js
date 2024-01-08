01eac551d3898d24ffc71098bc0eb0c23e57d061
MungaiS <thukusteven@gmail.com>
2024-01-08 11:42:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+function moveDodgerLeft() {
+  dodger.style.left = (parseInt(dodger.style.left) || 0) - 10 + 'px';
+}
+
+function moveDodgerRight() {
+  dodger.style.left = (parseInt(dodger.style.left) || 0) + 10 + 'px';
+}
+
